fa8f0f45f3e44025096533766a8e4ea89493a8dc
annastockelid <annastockelid@live.se>
2020-04-27 10:58:14 +0200

    Tidigare projekt

---
+++ BActeria Destruction 3D/Assets/Bacteria.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bacteria : MonoBehaviour
+{
+
+    public GameObject bacteriaSlicedPrefab;
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if(col.tag == "Blade")
+        {
+            Instantiate(bacteriaSlicedPrefab);
+            Destroy(gameObject);
+        }
+    }
+}
