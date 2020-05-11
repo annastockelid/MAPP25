a55b69aafba7a4d2c5d108eb21b37e21e8527606
annastockelid <annastockelid@live.se>
2020-05-11 16:38:33 +0200

    HighScore

---
+++ BActeria Destruction 3D/Assets/HighScoreScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreScript : MonoBehaviour
+{
+    public int scoreValue;
+    public Text highScore;
+
+    void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+
+   
+    void Update()
+    {
+        if (scoreValue > PlayerPrefs.GetInt("HighScore", 0)) 
+        {
+            PlayerPrefs.SetInt("HighScore", scoreValue);
+            highScore.text = scoreValue.ToString();
+        }
+ 
+    }
+}
