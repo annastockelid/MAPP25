046588f6c5ef78f6da0f019a89372700b1b4f669
emmawellmar <emma@wellmar.se>
2020-05-13 22:32:08 +0200

    bomb

--- BActeria Destruction 3D/Assets/Scripts/BombSpawner.cs
+++ BActeria Destruction 3D/Assets/Scripts/BombSpawner.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FruitSpawner : MonoBehaviour
+public class BombSpawner : MonoBehaviour
 {
 
     public GameObject fruitPrefab;
--- Emma Delkomponent/Emma Delkomponent/Assets/Scripts/Bomb.cs
+++ Emma Delkomponent/Emma Delkomponent/Assets/Scripts/Bomb.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fruit : MonoBehaviour
+public class Bomb : MonoBehaviour
 {
 
     public GameObject fruitSlicedPrefab;
