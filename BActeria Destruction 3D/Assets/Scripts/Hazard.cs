264314c62f2f14c7105457fb98b0b2f04f4547b8
annastockelid <annastockelid@live.se>
2020-05-25 11:47:44 +0200

    screenshake

--- BActeria Destruction 3D/Assets/Scripts/Hazard.cs
+++ BActeria Destruction 3D/Assets/Scripts/Hazard.cs
@@ -7,19 +7,22 @@ public class Hazard : MonoBehaviour
 
     public GameObject hazardSlicedPrefab;
 
+
     Rigidbody2D rb;
     public float startForce = 15f;
 
-    private void Start()
+    void Start()
     {
+
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
+   void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Blade")
         {
+
             ScoreScript.scoreValue -= 1; //Lägger till 1 på poäng
 
             Vector3 direction = (col.transform.position - transform.position).normalized;
