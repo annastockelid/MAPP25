d7b92517ace1e70b0d1326e1e4ef0f13fc967f63
annastockelid <annastockelid@live.se>
2020-05-28 14:42:54 +0200

    leaderboard

---
+++ BActeria Destruction 3D/Assets/Scripts/LeaderBoardEntryData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DapperDino.LeaderBoards
+{
+    public struct LeaderBoardEntryData 
+    {
+        public string enrtyName
+    }
+}
+
